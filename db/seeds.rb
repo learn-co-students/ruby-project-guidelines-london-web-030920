d894fbbdbbbb2a9fdeebea971439ddb198406b83
Isak <kallenbach13@gmail.com>
2020-03-24 15:11:10 +0000

    added characters and changed four wedding to truman

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,27 +19,19 @@ Movie.create(title: "Blues Brothers", year: 1980, genre: "Comedy")
 Movie.create(title: "Wild Child", year: 2008, genre: "Rom-Com")
 
 Character.create(first_name: "Truman" , last_name: "Burbank" , sex: "M" )
-
 Character.create(first_name: "Hannah" , last_name: "Gill" , sex: "F" )
 
 Character.create(first_name: "Luke" , last_name: "Skywalker" , sex: "M" )
-
 Character.create(first_name: "Darth" , last_name: "Vader" , sex: "M" )
 
 Character.create(first_name: "Ennis" , last_name: "Del Mar" , sex: "M" )
-
 Character.create(first_name: "Jack" , last_name: "Twist" , sex: "M" )
 
 Character.create(first_name: "Brennan" , last_name: "Huff" , sex: "M" )
-
 Character.create(first_name: "Dale" , last_name: "Doback" , sex: "M" )
 
 Character.create(first_name: "Ron" , last_name: "Burgundy" , sex: "M" )
-
 Character.create(first_name: "Veronica" , last_name: "Corningstone" , sex: "F" )
-
-# Quote.create(line: "malibu moment", movie_id: 10, character_id: 1)
-
 
 Character.create(first_name: "Peter", last_name: "Parker", sex: "M")
 Character.create(first_name: "Mary", last_name: "Watson", sex: "F")
