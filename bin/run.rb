f58393caa85a0fb8bd3ca63fd1d58b3820385750
Premti Krasniqi <premti@gmail.com>
2020-03-25 14:46:44 +0000

    Method for most expensive player

--- bin/run.rb
+++ bin/run.rb
@@ -1,4 +1,14 @@
 require_relative '../config/environment'
+player_1 = Player.first
+player_2 = Player.second
+player_3 = Player.third
+player_4 = Player.fourth
+team_1 = Team.first
+team_2 = Team.second
+contract_1 = Contract.first
+contract_2 = Contract.second
+contract_3 = Contract.third
+contract_4 = Contract.fourth
 
 binding.pry
 
